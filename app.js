461cef727a5b7711dafee959b7128cc1bafbb825
Andrew <messerback@hotmail.com>
2018-05-14 12:32:00 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.send("<h1>Working fine</h1>");
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Up and running on port ${port}`);
+})
