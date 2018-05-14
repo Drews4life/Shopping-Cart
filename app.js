4e654444c74d3ea0db19c66ce7a13adc8d65d763
Andrew <messerback@hotmail.com>
2018-05-14 13:07:57 +0300

    HTML and views set-up

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 
+
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -11,7 +12,7 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Working fine</h1>");
+    res.render("index");
 })
 
 
